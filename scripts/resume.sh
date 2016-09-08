5a45390c898eda8cfb693877d66c7bc58558a6dd
apatedolos <apatedolos@dread-it.com>
2016-09-08 13:59:03 +0100

    Create resume.sh

---
+++ scripts/resume.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Bash script to resume 1 Master and n slaves
+
+proc=$1
+outdir=$2
+bin=$3
+
+#if less than three arguments supplied display usage
+ 
+if [ $# -le 2 ]
+then
+        echo "Usage: $0 cores input-dir output-dir binary+commands"
+        echo 'Example Usage: '$0' 38 in-dir out-dir "./bash -c :"'
+        exit 1
+fi
+
+screen -S master -d -m /usr/local/bin/afl-fuzz -i- -o $outdir -M master -- $bin 
+
+for ((i = 1; i <= $1; i++)); do
+    screen -S slave-$i -d -m afl-fuzz -i- -o $outdir -S slave-$i -- $bin
+done
